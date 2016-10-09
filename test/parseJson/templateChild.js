b07c5dd6f0cb391b18d8115d51131bc416998083
Jim Argeropoulos <tenholeharp@gmail.com>
2016-10-08 23:28:17 -0400

    Template children are working

---
+++ test/parseJson/templateChild.js
@@ -0,0 +1,20 @@
+import test from 'ava'
+import sut from '../../lib/firebase-path'
+
+test('template child is a function', t => {
+    const children = sut.parseJson({'<fruit>': {}})
+
+    t.is(typeof(children.fruit), 'function')
+})
+
+test('template child path is filled in at path', t => {
+    const children = sut.parseJson({'<fruit>': {}})
+
+    t.is(children.fruit('bannana').path(), 'bannana')
+})
+
+test('template child followed by named node path', t => {
+    const children = sut.parseJson({'<foo>': {bar:{}}})
+
+    t.is(children.foo('phoo').bar.path(), 'phoo/bar')    
+})
