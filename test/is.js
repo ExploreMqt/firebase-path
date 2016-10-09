9ba73ee606c16b7fe94565945f9081b91fe47c36
Jim Argeropoulos <tenholeharp@gmail.com>
2016-10-08 23:42:54 -0400

    Filled out the remainder of the prototype check utility functions

--- test/is.js
+++ test/is.js
@@ -16,3 +16,43 @@ test('isRoot is true for empty', t => {
 test('isRoot is true for empty', t => {
     t.truthy(sut.isRoot(sut.parseJson({foo:{}})))
 })
+
+test('isRoot is false for first child', t => {
+    const children = sut.parseJson({foo:{}})
+
+    t.falsy(sut.isRoot(children.foo))
+})
+
+
+
+test('has isChild function', t => {
+    t.is(typeof(sut.isChild), 'function')
+})
+
+test('isChild is false for root', t => {
+    t.falsy(sut.isChild(sut.parseJson()))
+})
+
+test('isChild is true for first child', t => {
+    const children = sut.parseJson({foo:{}})
+
+    t.truthy(sut.isChild(children.foo))
+})
+
+
+
+test('has isPath function', t => {
+    t.is(typeof(sut.isPath), 'function')
+})
+
+test('isPath is true for root', t => {
+    const children = sut.parseJson({foo:{}})
+
+    t.truthy(sut.isPath(children))
+})
+
+test('isPath is true for child', t => {
+    const children = sut.parseJson({foo:{}})
+
+    t.truthy(sut.isPath(children.foo))
+})
